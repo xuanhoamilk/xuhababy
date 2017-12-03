66d02d62faf9a3dab17aa7e35eaa4b8b840b37fc
xuanhoamilk <32384027+phannhuson@users.noreply.github.com>
2017-12-03 23:30:24 +0700

    revise all
    revise all

--- tainguyen/js/index.js
+++ tainguyen/js/index.js
@@ -49,7 +49,7 @@ var evt = new Event();
     var m = new Magnifier(evt);
     m.attach({
         thumb: '#thumb',
-        large: 'img1/chuafix/6tragiamcanvytea.jpg',
+        large: 'img1/chuafix/7tragiamcanhoasamdat.jpg',
         largeWrapper: 'preview',
         zoom: 3,
         zoomable: true
