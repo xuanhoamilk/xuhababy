bba763f62d4aa348e7a042a5c9311c21f09381e3
xuanhoamilk <32384027+phannhuson@users.noreply.github.com>
2017-12-04 13:43:21 +0700

    a
    a

--- tainguyen/js/index.js
+++ tainguyen/js/index.js
@@ -44,13 +44,3 @@ $(window).scroll(function() {
 
 
 
-/*ham cho zoom*/
-var evt = new Event();
-    var m = new Magnifier(evt);
-    m.attach({
-        thumb: '#thumb',
-        large: 'img1/chuafix/7tragiamcanhoasamdat.jpg',
-        largeWrapper: 'preview',
-        zoom: 3,
-        zoomable: true
-    });
