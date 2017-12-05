b7cfb638a51b7c20df272238a142a72b9bcf64be
xuanhoamilk <32384027+phannhuson@users.noreply.github.com>
2017-12-05 09:29:26 +0700

    all
    all

--- tainguyen/js/zoom03vinaga.js
+++ tainguyen/js/zoom03vinaga.js
@@ -3,7 +3,7 @@ var evt = new Event();
     var m = new Magnifier(evt);
     m.attach({
         thumb: '#thumb',
-        large: 'img1/chuafix/7tragiamcanhoasamdat.jpg',
+        large: 'img1/chuafix/2vinaga.jpg',
         largeWrapper: 'preview',
         zoom: 3,
         zoomable: true
